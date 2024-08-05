8cf02bce91e01d46d08a6e458694cb6ed20d179a
TurtleRuss <tttturtleruss@gmail.com>
2024-08-06 03:44:04 +0800

    pass 3st stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
 
+const std::string alphaBet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+
 bool match_pattern(const std::string& input_line, const std::string& pattern) {
     if (pattern.length() == 1) {
         return input_line.find(pattern) != std::string::npos;
     }else if (pattern == "\\d"){
         return input_line.find_first_of("0123456789") != std::string::npos;
+    }else if (pattern == "\\w") {
+        return input_line.find_first_of(alphaBet) != std::string::npos;
     }else {
         throw std::runtime_error("Unhandled pattern " + pattern);
     }
