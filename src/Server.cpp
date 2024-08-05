85b0f61d02dd3afbf1fed594b08f061a3cbd4113
TurtleRuss <tttturtleruss@gmail.com>
2024-08-06 03:40:12 +0800

    pass 2st stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -4,8 +4,9 @@
 bool match_pattern(const std::string& input_line, const std::string& pattern) {
     if (pattern.length() == 1) {
         return input_line.find(pattern) != std::string::npos;
-    }
-    else {
+    }else if (pattern == "\\d"){
+        return input_line.find_first_of("0123456789") != std::string::npos;
+    }else {
         throw std::runtime_error("Unhandled pattern " + pattern);
     }
 }
